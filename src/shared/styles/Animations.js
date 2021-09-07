bf31e4cd0504da2f6808866dedbdf173ceac4280
Gin <gintautas.anduzis@gmail.com>
2021-09-07 18:05:55 +0300

    remove unused animations

--- src/shared/styles/Animations.js
+++ src/shared/styles/Animations.js
@@ -6,16 +6,6 @@ export const transition = (property, duration) => css`
   transition: ${property} ${duration}ms ${cubicBezier} 0ms;
 `;
 
-export const enterFromRight = keyframes`
-  0% {
-            transform: scaleX(0.2);
-            transform-origin: 100% 100%;
-  }
-  100% {
-            transform: scaleX(1);
-            transform-origin: 100% 100%;
-  }
-`;
 export const warningLeft = keyframes`
   0% {
     transform: translateX(48px);
@@ -49,31 +39,5 @@ export const warningLeft = keyframes`
     transform: translateX(0px);
     animation-timing-function: ease-out;
     opacity: 1;
-  }
-`;
-
-export const enterElement = keyframes`
-  from {
-
-    filter: blur(0.2rem);
-    transform: translateZ(0);
-  }
-  to {
-
-    filter: blur();
-    transform: translateZ(0);
-  }
-`;
-export const loading = keyframes`
-  0% { 
-    transform: perspective(120px) rotateX(0deg) rotateY(0deg) ;
-    opacity: 1;
-  } 50% { 
-    transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg) ;
-    opacity: 1;
-  } 100% { 
-    transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg) ;
-    opacity: 1;
-
   }
 `;
