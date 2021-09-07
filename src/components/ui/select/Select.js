b39a92e80db06565c336cb48cf40cab7d4ea10ef
Gin <gintautas.anduzis@gmail.com>
2021-09-07 18:07:18 +0300

    key :)

--- src/components/ui/select/Select.js
+++ src/components/ui/select/Select.js
@@ -3,8 +3,10 @@ import * as s from './styles';
 const Select = ({ data, defaultValue, handleSelect }) => {
   return (
     <s.Select value={defaultValue} onChange={handleSelect}>
-      {data.map((el) => (
-        <option value={el}>{el}</option>
+      {data.map((el, index) => (
+        <option key={index} value={el}>
+          {el}
+        </option>
       ))}
     </s.Select>
   );
